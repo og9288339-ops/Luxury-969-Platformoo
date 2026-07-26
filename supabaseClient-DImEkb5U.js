7241ba8c330eaac0dae47207362702514e79c66d
og9288339-ops <og9288339@gmail.com>
2026-07-27 00:43:35 +0300

    Add files via upload

---
+++ supabaseClient-DImEkb5U.js
@@ -0,0 +1,1 @@
+import{ai as s}from"./vendor-kXDRGFHP.js";const i=s("https://eakpvbgejctdxgqrtyit.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVha3B2YmdlamN0ZHhncXJ0eWl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxMDU1MDUsImV4cCI6MjA5ODY4MTUwNX0.TM-05HcgfbpC525y86VB5wOqi5NORd0tmf8eFaLrxlo",{auth:{persistSession:!0,autoRefreshToken:!0}});export{i as s};
